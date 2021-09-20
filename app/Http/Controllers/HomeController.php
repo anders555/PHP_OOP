2c9691861fa7a41455886f1850e969af72da5979
Andrei Lashkevich <lashkevich95@gmail.com>
2021-09-20 09:12:05 +0300

    laravel model+factory+seeder+migration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lead;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
+        Product::create([
+            'name'=> 'new product',
+            'description'=> 'new product desc',
+            'price'=>100,
+        ]);
+        Lead::create([
+            'name'=> 'new name',
+            'query'=> 'new  query',
+            'tel'=>'23146237462',
+            'processed'=>'false',
+        ]);
         return view('home.main');
+
     }
     public function aboutUs(){
         return view('home.about');
---
+++ database/migrations/2021_09_19_195000_employees.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Employees extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name', 255)->nullable(false);
+            $table->string('tel',50);
+            $table->text('about')->nullable();
+            $table->text('at_work');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
